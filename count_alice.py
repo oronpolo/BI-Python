8e6cb44318183f3ed1f47c778f520e3f1eb39f59
oronpolo <oronpolo@gmail.com>
2024-11-04 15:26:35 +0200

    ver3

--- count_alice.py
+++ count_alice.py
@@ -1,24 +1,43 @@
 #   This program will find the word that appears the most in Alice's Adventures in Wonderland
 
 
+def read_Alice_text(file_path):
+            #file = open(file_path, "r", encoding='UTF8')
+            #file_content = file.read()
+            #return file_content
+            print('read Alice')
+    
+
+def clean_Alice():
+        #clean_a()
+        #clean_b()
+        print('clean Alice')
+
+def count_words_in_Alice():
+        print('count Alice')
+    
+   
+
+#print(read_Alice_text("D:/OneDrive/studies/Bar-Ilan/pyton/BI-Python/alice_in_wonderland.txt"))
+
+
+
+Alice_Path = "D:/OneDrive/studies/Bar-Ilan/pyton/BI-Python/alice_in_wonderland.txt"
 
 def main_count_Alice():
-    alice_text = read_Alice_text()
-    clean_alice_text = clean_Alice(alice_text)
+    Alice_text = read_Alice_text(Alice_Path)
+    Alice_words = clean_Alice(Alice_text)
+    (words_dict, max_word_count) = count_words_in_Alice(Alice_words)
 
-    def read_Alice_text(text_path):
-            content = open(text_path)
-            content = open(text_path)
-            return content.read()
-    
 
-    def clean_Alice():
-        pass
-    
-    splitted_text = read_Alice_text("/home/oron/github/BI-Python/BI-Python/alice_in_wonderland.txt")
+    main_count_Alice()
 
-    print(splitted_text)
+
 
 
     
+
+
+
+
 
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 # Program to read the entire file using read() function
-file = open("p/home/oron/github/BI-Python/BI-Python/alice_in_wonderland.txt", "r")
+file = open("d:/OneDrive/studies/Bar-Ilan/pyton/BI-Python/text_file.txt", "r", encoding='UTF8')
 content = file.read()
 print(content)
 file.close()
