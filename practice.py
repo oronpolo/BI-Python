24bf391fd22707f3c677b98bdd81de37928b3456
oronpolo <122163179+oronpolo@users.noreply.github.com>
2024-11-04 19:30:31 +0000

    rev4

---
+++ practice.py
@@ -0,0 +1,11 @@
+#Exercise 1: Calculate the multiplication and sum of two numbers
+def get_numbers_from_user():
+    num1, num2 = input("please enter two numbers").split()
+    mult = num1 * num2
+    print(mult)
+
+get_numbers_from_user()
+
+nums = [int(n) for n in text.split()]
+    
+  
