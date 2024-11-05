0d9a86f07ebf3c396d5b05a44cdfb85368ebd1ca
oronpolo <122163179+oronpolo@users.noreply.github.com>
2024-11-05 18:20:45 +0000

    rev6

--- practice.py
+++ practice.py
@@ -2,27 +2,27 @@
 #    num1, num2 = map(int, input("please enter two numbers\n" ).split())
 #    num1, num2 = input("please enter two numbers\n" ).split()
 #    num1, num2 = int(num1), int(num2)
-    while (True):
+while (True):
         try:
             num1 = int(input("please enter the first number\n" ))
             break
         except ValueError:
             print("please enter an integer number")
             
-    while True:
+while True:
         try:
             num2 = int(input("please enter the second number\n" ))
             break
         except ValueError:
             print("please enter an integer number")
     
-    mult = num1 * num2
-    print(mult)
+mult = num1 * num2
+print(mult)
 
 get_numbers_from_user()
 
 def check_input_is_numeric(number):
-    if(type(number) == int or float):
+    if(type(number) == int or type(number) == float):
         return True
     else:
         return False
