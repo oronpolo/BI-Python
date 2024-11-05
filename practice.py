ce6fb770f1f475b562890ad1bee6eef7245f39eb
oronpolo <122163179+oronpolo@users.noreply.github.com>
2024-11-05 18:18:34 +0000

    rev5

--- practice.py
+++ practice.py
@@ -1,11 +1,30 @@
-#Exercise 1: Calculate the multiplication and sum of two numbers
-def get_numbers_from_user():
-    num1, num2 = input("please enter two numbers").split()
+#def get_numbers_from_user():
+#    num1, num2 = map(int, input("please enter two numbers\n" ).split())
+#    num1, num2 = input("please enter two numbers\n" ).split()
+#    num1, num2 = int(num1), int(num2)
+    while (True):
+        try:
+            num1 = int(input("please enter the first number\n" ))
+            break
+        except ValueError:
+            print("please enter an integer number")
+            
+    while True:
+        try:
+            num2 = int(input("please enter the second number\n" ))
+            break
+        except ValueError:
+            print("please enter an integer number")
+    
     mult = num1 * num2
     print(mult)
 
 get_numbers_from_user()
 
-nums = [int(n) for n in text.split()]
+def check_input_is_numeric(number):
+    if(type(number) == int or float):
+        return True
+    else:
+        return False
     
   
