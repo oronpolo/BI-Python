2eadf971521f04abeb80d09e1e914f39f8fcf2fe
oronpolo <oronpolo@gmail.com>
2024-11-14 18:25:32 +0200

    rev 8

--- count_alice.py
+++ count_alice.py
@@ -1,17 +1,17 @@
 #   This program will find the word that appears the most in Alice's Adventures in Wonderland
+from useful_func import *
 
-
-def read_Alice_text(file_path):
-            #file = open(file_path, "r", encoding='UTF8')
-            #file_content = file.read()
-            #return file_content
-            print('read Alice')
-    
-
-def clean_Alice():
+def clean_Alice(txt:str):
+       text = ToLower(txt)
+       text_index = 1
+       #while(text_index != -1):
+       #      text = text.replace("  ", " ")
+       #      text_index = text.find("  ")
+       #Clean_text ="".join(c for c in text if c == " " elif c.isalpha())
+       #txt ="".join(c for c in txt if c.isalpha())
+       #return Clean_text
         #clean_a()
         #clean_b()
-        print('clean Alice')
 
 def count_words_in_Alice():
         print('count Alice')
@@ -22,17 +22,23 @@ def count_words_in_Alice():
 
 
 
-Alice_Path = "D:/OneDrive/studies/Bar-Ilan/pyton/BI-Python/alice_in_wonderland.txt"
+Alice_Path = 'D:\\OneDrive\\studies\\Bar-Ilan\\pyton\\BI-Python\\alice_in_wonderland.txt'
 
 def main_count_Alice():
-    Alice_text = read_Alice_text(Alice_Path)
-    Alice_words = clean_Alice(Alice_text)
-    (words_dict, max_word_count) = count_words_in_Alice(Alice_words)
+    Alice_text = read_txt_file_content(Alice_Path)
+    #Alice_words = clean_Alice(Alice_text)
+    #(words_dict, max_word_count) = count_words_in_Alice(Alice_words)
+    print(Alice_text)
+    Alice_text = Alice_text.split
+    print(Alice_text)
+
+#main_count_Alice()
 
 
-    main_count_Alice()
-
-
+print(clean_Alice("@!A  bCd   Ef      G$%^"))
+txt = "@!A  bCd   Ef      G$%^".split()
+#txt = str(txt)
+print(type(txt))
 
 
     
--- useful_func.py
+++ useful_func.py
@@ -17,4 +17,18 @@ def get_user_input():
                 val = str(user_input)
                 return val
 
+def read_txt_file_content(file_path):
+    file = (open(file_path,'r', encoding='UTF8'))
+    file_content = file.read()
+    return file_content
+
+def Clean(txt:str):
+       text = ToLower(txt)
+       #txt.replace('  ', ' ')
+       #txt ="".join(c for c in txt if c.isalpha())
+       return text
+
+def ToLower(txt:str):
+    x = txt.lower()
+    return x
 
