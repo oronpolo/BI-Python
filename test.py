94e805a29f197622ce52478e80de613df53393f3
John Doe <johndoe@email.com>
2024-11-22 20:18:52 +0200

    rev 9

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
+text = "@#$ AbVc# asd fdfggf *&* # "
+print (text)
+clean_text = ""
+#for i in text:
+#    x = text[i]
+#    x = str(x)
 
-def main(num):
-    temp = 0
-    for i in range (1, num+1):
-        temp += i
-    print(temp)
 
-main(10)
+
+       
+
+#transfer text to list
+text_list = text.split()
+print(text_list)
+
+
+#transfer text back to string
+text3 = " ".join(text)
+print(text3)
